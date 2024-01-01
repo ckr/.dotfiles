418432977c486f1e7539f6586fbd77f325f5b95e
Constantinos Kouloumbris <c@kouloumbris.com>
2024-01-01 19:55:37 +0200

    added greeter dashboard

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -41,4 +41,13 @@ return {
 
   -- git integration
   'lewis6991/gitsigns.nvim', -- show line modifications on left hand side
+  -- neovim greeter dashboard
+  {
+    'goolord/alpha-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+      -- require'alpha'.setup(require'alpha.themes.dashboard'.config)
+      require'alpha'.setup(require'alpha.themes.startify'.config)
+    end
+  },
 }
