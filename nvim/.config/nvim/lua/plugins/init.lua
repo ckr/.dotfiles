c169da2fb19ab3aba7a332d9cc3441b49cb6eb27
Constantinos Kouloumbris <c@kouloumbris.com>
2024-01-13 21:53:58 +0000

    added toggleterm

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -60,5 +60,24 @@ return {
         -- add any options here
     },
     lazy = false,
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    lazy = false,
+    config = function()
+      require("toggleterm").setup({
+        close_on_exit = false,
+        autochdir = true,
+        shell = vim.o.shell,
+        size = 20,
+        direction = 'float',
+        terminal_mappings = true,
+        open_mapping = [[<C-\>]]
+      })
+    end,
+    -- keys = {
+    --   { [[<C-\>]], '<cmd>ToggleTerm direction=float<cr>' },
+    -- }
   }
 }
