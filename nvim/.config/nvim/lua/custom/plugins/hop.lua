6c5b6e0bd70a38cd8a6c0a8a94a7254e56c28cdd
Constantinos Kouloumbris <c@kouloumbris.com>
2024-03-13 21:50:01 +0000

    added hop to help with vim motions

---
+++ nvim/.config/nvim/lua/custom/plugins/hop.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "smoka7/hop.nvim",
+    opts = {
+      multi_windows = true,
+      keys = "htnsueoaidgcrlypmbkjvx",
+      uppercase_labels = true,
+    },
+    keys = {
+      {
+        "<leader>fj",
+        function()
+          require("hop").hint_words()
+        end,
+        desc = "Show vim motion key bindings",
+        mode = { "n", "x", "o" },
+      },
+    },
+  },
+}
