2d2ffaaf989a8673e4186deecb6b53e10d85c6c8
Constantinos Kouloumbris <c@kouloumbris.com>
2025-06-11 13:21:25 +0100

    more bar colors

--- sketchybar/.config/sketchybar/colors.sh
+++ sketchybar/.config/sketchybar/colors.sh
@@ -13,7 +13,10 @@ export ORANGE=0xfff5a97f
 export MAGENTA=0xffc6a0f6
 export GREY=0xff939ab7
 export TRANSPARENT=0x00000000
-export BG0TR50=0xa81e1e2e
+export BG0TR30=04d1e1e2e
+export BG0TR40=0x661e1e2e
+export BG0TR50=0x871e1e2e
+export BG0TR60=0xa81e1e2e
 export BG0=0xff1e1e2e
 export BG1=0x603c3e4f
 export BG2=0x60494d64
@@ -26,7 +29,7 @@ export BATTERY_4=0xffee99a0
 export BATTERY_5=0xffed8796
 
 # General bar colors
-export BAR_COLOR=$BG0TR50
+export BAR_COLOR=$BG0TR40
 export BAR_BORDER_COLOR=$TRANSPARENT
 export BACKGROUND_1=$BG1
 export BACKGROUND_2=$BG3
@@ -35,4 +38,3 @@ export LABEL_COLOR=$WHITE # Color of all labels
 export POPUP_BACKGROUND_COLOR=$BG0
 export POPUP_BORDER_COLOR=$WHITE
 export SHADOW_COLOR=$BLACK
-
