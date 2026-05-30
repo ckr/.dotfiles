d1f553928181bd335fd213d5c96a0a0ddf45e71e
Constantinos Kouloumbris <c@kouloumbris.com>
2026-05-30 13:56:12 +0100

    python formating

--- nvim/.config/nvim/lua/custom/plugins/autoformat.lua
+++ nvim/.config/nvim/lua/custom/plugins/autoformat.lua
@@ -9,6 +9,7 @@ return {
       },
       formatters_by_ft = {
         lua = { "stylua" },
+        python = { "ruff_fix", "ruff_format" },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
--- nvim/.config/nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/plugins/lspconfig.lua
@@ -134,7 +134,8 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        ruff = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -189,6 +190,8 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         "stylua", -- Used to format lua code
+        "ruff",
+        "pyright",
       })
       require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
